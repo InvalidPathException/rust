5d531c44c94e45ef9100c97316c3bf6080f38c97
Mark Z. Ding <invalidpathexception@gmail.com>
2026-07-12 00:25:51 -0400

    Add pretty test for non-doc comments following match arm attributes

---
+++ tests/pretty/match-arm-comment.pp
@@ -0,0 +1,14 @@
+//@ compile-flags: --crate-type=lib
+//@ pp-exact:match-arm-comment.pp
+
+// A non-doc comment following an attribute on a match arm must stay after the
+// attribute, not be reordered before it.
+fn f(x: u8) {
+    match x
+        {
+            #[cfg(all())]
+            // this non-doc comment must stay after the attribute
+            0 => {}
+        _ => {}
+    }
+}
